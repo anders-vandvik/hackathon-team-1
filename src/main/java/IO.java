55daa1d3200d501f4ae533e8b4b3a38e51904fc7
girts.metlans <girts.metlans@visma.com>
2022-01-12 12:29:09 +0200

    Initial version for reading a file

--- src/main/java/IO.java
+++ src/main/java/IO.java
@@ -1,6 +1,28 @@
 package main.java;
 
+import main.java.problem.Problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class IO {
+
+    public Problem readFile(String problemFile) {
+        InputStream inputStream = IO.class.getResourceAsStream(problemFile);
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Problem();
+    }
 
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 package main.java;
 
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String args[]) {
+        IO io = new IO();
+        io.readFile("/main/resources/instances/me_at_the_zoo.in.txt");
     }
 }
