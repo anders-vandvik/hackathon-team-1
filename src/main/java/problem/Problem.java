557828fd1ddd5b3fa78f052421e00dfb2f9ce88e
Anders Vandvik <anders.h.vandvik@visma.com>
2022-01-12 10:46:55 +0100

    Started on IO

---
+++ src/main/java/problem/Problem.java
@@ -0,0 +1,13 @@
+package main.java.problem;
+
+import java.util.List;
+
+public class Problem {
+
+    public static List<Video> videos;
+    public static List<Cache> caches;
+    public static List<Endpoint> endpoints;
+    public static List<Request> requests;
+
+
+}
