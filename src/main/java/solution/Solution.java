933ae94f6bf5ee13e75d1b8a08fb5dbe96ff55d7
girts.metlans <girts.metlans@visma.com>
2022-01-12 15:36:29 +0200

    Add client to submit solution

---
+++ src/main/java/solution/Solution.java
@@ -0,0 +1,10 @@
+package main.java.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public String id;
+    public List<Server> serverList = new ArrayList<>();
+}
---
+++ src/main/java/solution/SolutionSubmissionRequest.java
@@ -0,0 +1,11 @@
+package main.java.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionSubmissionRequest {
+
+    public String name;
+
+    public List<Solution> solutions = new ArrayList<>();
+}
